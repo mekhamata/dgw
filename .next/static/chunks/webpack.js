90b24b87946a611aca18f2726aa0ab88d336d040
mekhamata <mekha.r99@gmail.com>
2023-06-26 03:20:10 +0300

    change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4339c244461b05ea"; }
+/******/ 		__webpack_require__.h = function() { return "4129aaa124eaf3b7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
