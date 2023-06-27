75688a4bf203ce7265e5c7276f0c6405e50afb30
mekhamata <mekha.r99@gmail.com>
2023-06-27 12:02:23 +0300

    fixed responsive

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d85f3351c563a353"; }
+/******/ 		__webpack_require__.h = function() { return "aebff788819ebded"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
