dd26323dd05d3d1efbdae6fdd211509e313729b1
mekhamata <mekha.r99@gmail.com>
2023-07-26 19:01:47 +0300

    edit env syntax

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -117,7 +117,7 @@ const Contact = () => {
         "service_y0z1qup",
         "template_dm1k45s",
         templateParams,
-        `${process.env.NEXT_PUBLIC_EMAILJS_PRIVATE}`
+        `${process.env.ENVIRONMENTAL_VARIABLES_NEXT_PUBLIC_EMAILJS_PRIVATE}`
       )
       .then(
         function (response) {
